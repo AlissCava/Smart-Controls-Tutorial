1183f7a0d6db2bba812b73f78619fdde96a25edc
AlissCava <cavalleraalissa@gmail.com>
2026-02-17 11:10:03 +0100

    Step 7: View Management

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,12 +1,16 @@
 sap.ui.define([
-	'sap/ui/core/UIComponent'
+        "sap/ui/core/UIComponent"
 ], function(UIComponent) {
-	"use strict";
-
-	return UIComponent.extend("sap.ui.demo.smartControls.Component", {
-		metadata: {
-			manifest: "json"
-		}
-	});
-
+    "use strict";
+    return UIComponent.extend("sap.ui.demo.smartControls.Component", {
+        metadata: {
+            manifest: "json"
+        },
+        init: function () {
+            UIComponent.prototype.init.apply(this, arguments);
+        },
+        destroy: function () {
+                UIComponent.prototype.destroy.apply(this, arguments);
+        }
+    });
 });
